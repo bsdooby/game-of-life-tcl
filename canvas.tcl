1288a4d9cac949f793751a42fb6ceb136f5e9948
Peter von Niederhäusern <pvonnied@gmail.com>
2020-11-12 22:33:43 +0100

    wip

---
+++ canvas.tcl
@@ -0,0 +1,25 @@
+#!/usr/bin/env wish
+
+package require Tk
+
+set gridSize 70
+set cellSize 5 
+
+tk::canvas .canvas -width [expr $gridSize * $cellSize - 1] -height [expr $gridSize * $cellSize -1]  -background white
+pack .canvas
+
+set k 0
+set l 0
+for {set i 0} {$i < [expr $gridSize * $cellSize]} {incr i $cellSize} {
+    for {set j 0} {$j < [expr $gridSize * $cellSize]} {incr j $cellSize} {
+        .canvas create rectangle [expr $i + 0] [expr $j + 0] [expr $i + $cellSize] [expr $j + $cellSize] \
+            -fill white -outline black -tags "x:$k;y:$l"
+        incr l
+    }
+    set l 0
+    incr k
+}
+
+# 
+.canvas itemconfigure "x:69;y:69" -fill black
+
--- main.tcl
+++ main.tcl
@@ -2,9 +2,9 @@
 
 package require Tk
 
-set gridSize 50 
+set gridSize 80
 set cellSize 5
-set updateSpeed 1 
+set updateSpeed 1
 
 set gridState 0
 
@@ -19,6 +19,8 @@ ttk::style configure Alive.TFrame -background black -borderwidth 0.5 -relief sun
 ttk::style configure Neighbour.TFrame -background red -borderwidth 0.5 -relief sunken
 ttk::style configure Center.TFrame -background green -borderwidth 0.5 -relief sunken
 
+tk::canvas .universe -width [expr $gridSize * $cellSize] -height [expr $gridSize * $cellSize] -background white
+
 wm title . "Game-of-Life: generation $generation"
 wm geometry . +450+0
 
@@ -27,11 +29,24 @@ set frameList [list]
 array set currentState {}
 array set nextState {}
 
+set k 0
+set l 0
+for {set i 0} {$i < [expr $gridSize * $cellSize]} {incr i $cellSize} {
+    for {set j 0} {$j < [expr $gridSize * $cellSize]} {incr j $cellSize} {
+        .universe create rectangle $i $j [expr $i + $cellSize] [expr $j + $cellSize] \
+            -fill white -outline black -tags "x:$l;y:$k" ;# follows column-major -> swap 
+        set currentState($k,$l) 0
+        incr l
+    }
+    set l 0
+    incr k
+}
+
 for {set i 0} {$i < $gridSize} {incr i} {
     lappend frameList [list] 
     for {set j 0} {$j < $gridSize} {incr j} {
         lset frameList $i end+1 [ttk::frame .cell-$i-$j -width $cellSize -height $cellSize -style Dead.TFrame]
-        
+       
         set currentState($i,$j) 0 
         set nextState($i,$j) 0 
 
@@ -41,7 +56,123 @@ for {set i 0} {$i < $gridSize} {incr i} {
     }
 }
 
+proc moore' {x y} {
+
+    #set ne nonewline
+    
+    global gridSize updateSpeed currentState nextState
+    
+    set neighbours [list]
+
+    # center cell
+    #.universe itemconfigure "x:$x;y:$y" -fill red
+    #update
+    #after $updateSpeed
+    #.universe itemconfigure "x:$x;y:$y" -fill white
+    #update
+    
+    set livingNeighbours 0
+    if {1} {
+        # north
+        #puts -$ne "{[expr $x-1] [expr $y+0]}"
+        set x_ [expr $x-1]
+        set x_ [expr $x_ < 0 ? $gridSize-1 : $x_]
+        lappend neighbours "$x_ $y"
+
+        # northeast
+        #puts -$ne "{[expr $x-1] [expr $y+1]}"
+        set y_ [expr $y+1]
+        set y_ [expr $y_ > $gridSize-1 ? 0 : $y_]
+        lappend neighbours "$x_ $y_"
+        
+        # east
+        #puts -$ne "{[expr $x-0] [expr $y+1]}"
+        lappend neighbours "$x $y_"
+        
+        # southeast
+        #puts -$ne "{[expr $x+1] [expr $y+1]}"
+        set x_ [expr $x+1]
+        set x_ [expr $x_ > $gridSize-1 ? 0 : $x_]
+        lappend neighbours "$x_ $y_"
+        
+        # south
+        #puts -$ne "{[expr $x+1] [expr $y+0]}"
+        lappend neighbours "$x_ $y"
+        
+        # southwest
+        #puts -$ne "{[expr $x+1] [expr $y-1]}"
+        set y_ [expr $y-1]
+        set y_ [expr $y_ < 0 ? $gridSize-1 : $y_]
+        lappend neighbours "$x_ $y_"
+        
+        # west
+        #puts -$ne "{[expr $x-0] [expr $y-1]}"
+        lappend neighbours "$x $y_"
+
+        # northwest
+        #puts "{[expr $x-1] [expr $y-1]}"
+        set x_ [expr $x-1]
+        set x_ [expr $x_ < 0 ? $gridSize-1 : $x_]
+        lappend neighbours "$x_ $y_"
+        
+        # count'em
+        foreach nc $neighbours {
+           
+            lassign [split $nc " "] x_ y_
+            if {$currentState($x_,$y_) == 1} {
+                incr livingNeighbours
+            }
+        }
+
+        # show sliding window
+        if {0} {
+            foreach nc $neighbours {
+                lassign [split $nc " "] x_ y_
+                .universe itemconfigure "x:$x_;y:$y_" -fill green
+            }
+            update
+
+            after $updateSpeed
+
+            foreach nc $neighbours {
+                lassign [split $nc " "] x_ y_
+                .universe itemconfigure "x:$x_;y:$y_" -fill white
+            }
+            update
+        }
+
+        # rule engine
+
+        # rule 1:
+	    # any live cell with two or three live neighbours survives
+
+        if {$currentState($x,$y) == 1} {
+            switch $livingNeighbours {
+                2 -
+                3 {
+                    set nextState($x,$y) 1
+                }
+                default {
+                    set nextState($x,$y) 0
+                }
+            }
+       
+       # rule 2:
+	   # any dead cell with three live neighbours becomes a live cell
+            
+       } else {
+            if {$livingNeighbours == 3} {
+                set nextState($x,$y) 1
+            }
+        } 
+
+        # rule 3:
+	    # all other live cells die in the next generation. Similarly, all other dead cells stay dead.			
+    }
+}
+
 proc moore {cell} {
+ 
     #set ne nonewline
     
     global gridSize updateSpeed frameList currentState nextState
@@ -108,6 +239,7 @@ proc moore {cell} {
             }
         }
 
+        # show sliding window
         if {0} {
             foreach nc $neighbours {
                 $nc configure -style Neighbour.TFrame
@@ -157,7 +289,22 @@ proc moore {cell} {
     }
     #after $updateSpeed
     #.cell-$x-$y configure -style Dead.TFrame
-    #update
+    update
+}
+
+proc update_' {x y} {
+ 
+    global currentState nextState gridState
+
+    set currentState($x,$y) $nextState($x,$y)
+    set nextState($x,$y) 0
+
+    if {$currentState($x,$y) == 1} {
+        .universe itemconfigure "x:$x;y:$y" -fill black
+    } else {
+        .universe itemconfigure "x:$x;y:$y" -fill white
+    }
+    update
 }
 
 proc update_ {cell} {
@@ -171,19 +318,60 @@ proc update_ {cell} {
 
     if {$currentState($x,$y) == 1} {
         .cell-$x-$y configure -style Alive.TFrame
-        #incr gridState
     } else {
         .cell-$x-$y configure -style Dead.TFrame
-        #incr gridState -1
     }
     update
+}
+
+proc main' {} {
+
+    global gridSize cellSize currentState seed generation
+
+    pack .universe
+
+    # init seed cells
+    for {set i 0} {$i < $seed} {incr i} {
+        set x [expr int(rand()*$gridSize)]
+        set y [expr int(rand()*$gridSize)]
+        set currentState($x,$y) 1 
+    }
+
+    # set seed cells
+    for {set i 0} {$i < $gridSize} {incr i} {
+        for {set j 0} {$j < $gridSize} {incr j} {
+            set state $currentState($i,$j)
+            if {$state == 1} {
+                .universe itemconfigure "x:$i;y:$j" -fill black
+            }
+            update
+        }
+    }
+
+    # enter the game loop
+    while {1} {
+        for {set i 0} {$i < $gridSize} {incr i} {
+            for {set j 0} {$j < $gridSize} {incr j} {
+                moore' $i $j
+            }
+        }
+ 
+        wm title . "Game-of-Life: generation $generation"
+        incr generation
+
+        for {set i 0} {$i < $gridSize} {incr i} {
+            for {set j 0} {$j < $gridSize} {incr j} {
+                update_' $i $j
+            }
+        }
+    }
 }
 
 proc main {} {
 
     global seed gridSize frameList currentState generation gridState
 
-    # seed cells
+    # init seed cells
     for {set i 0} {$i < $seed} {incr i} {
         set x [expr int(rand()*$gridSize)]
         set y [expr int(rand()*$gridSize)]
@@ -199,7 +387,7 @@ proc main {} {
     #set currentState(15,5) 1
     #set currentState(16,5) 1
 
-    # init cell states
+    # set seed cells
     if {1} {
         for {set i 0} {$i < $gridSize} {incr i} {
             for {set j 0} {$j < $gridSize} {incr j} {
@@ -237,7 +425,6 @@ proc main {} {
         }
  
         wm title . "Game-of-Life: generation $generation"
-        #puts "generation: $generation"
         incr generation
 
         foreach l $frameList {
@@ -245,11 +432,9 @@ proc main {} {
                 update_ $c
             }
         }
-
-        #puts "state: $gridState"
     }
 }
 
 # start
-main 
+main
 
